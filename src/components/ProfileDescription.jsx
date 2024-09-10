ff97542f4bb25b9358516d9d5364bd3fa3fa28e1
xximxxhatedxx <ghostr210305@gmail.com>
2024-09-10 19:19:14 +0200

    lab1.1

---
+++ src/components/ProfileDescription.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ProfileDescription() {
+    return (
+        <div>
+            <h1>User Name</h1>
+            <p>Description 1</p>
+        </div>
+    )
+  }
+
+export default ProfileDescription;
